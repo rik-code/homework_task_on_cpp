44cbb5a4413ad467d5a2c345f0b0367de84c8eb2
Alexey Rytsku <topelven1234567890@gmail.com>
2022-10-27 02:50:44 +0300

    Initial commit

---
+++ 20_10_2022/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+// задача №1
+void task_one() {
+    for (int i = 0; i < 20; i += 2) {
+        cout << i << endl;
+    }
+}
+
+// задача №2
+void task_two() {
+    int a[] = {3, 4, 1};
+    cout << a[0] * a[1] * a[2] << endl;
+}
+
