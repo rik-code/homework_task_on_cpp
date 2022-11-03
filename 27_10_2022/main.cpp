f42f34f29ccd48e52b0225c3a6336aae5d0420db
Alexey Rytsku <topelven1234567890@gmail.com>
2022-11-03 18:07:49 +0300

    дорешал 2 и 3 задачу

--- 27_10_2022/main.cpp
+++ 27_10_2022/main.cpp
@@ -2,6 +2,10 @@
 
 using namespace std;
 
+int randint (int min, int max) {
+    return rand() % (max - min) + min; // эту формулу нужно просто запомнить
+}
+
 // задача №1
 void task_1()
 {
@@ -14,3 +18,41 @@ void task_1()
     cout << sum << endl;
 }
 
+// задача №2
+void task_2() {
+    int array[100];
+    int neg = 0, pos = 0, even = 0;
+    for (int i = 0; i < 100; i++) {
+        array[i] = randint(-20, 100);
+        cout << array[i] << endl;
+    }
+    cout << endl;
+    for (int i = 0; i < 100; i++) {
+        if (array[i] < 0) {
+            neg++;
+        }
+        if (array[i] > 0){
+            pos++;
+        }
+        if (array[i] % 2 == 0) {
+            even++;
+        }
+    }
+    cout << neg << endl;
+    cout << pos << endl;
+    cout << even << endl;
+}
+
+// задача №3
+void task_3() {
+    int array[100];
+    int sum=0;
+    for (int i = 0; i < 100; i++) {
+        array[i] = randint(-20, 100);
+        cout << array[i] << endl;
+    }
+    for (int i = 0; i < 10; i++) {
+        sum+=array[i];
+    }
+    cout << sum << endl;
+}
